1abb79277406bc65ac45fd72366653584b530127
cplasfwst <2407850@qq.com>
2023-04-06 14:32:47 +0800

    准备开始使用route

--- src/App.vue
+++ src/App.vue
@@ -1,154 +1,27 @@
 <template>
   <div class="container">
     <GGGlobalHeader :user="currentUser"></GGGlobalHeader>
-    <ValidateForm @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label"
-          >邮箱地址(参考，不用)</label
-        >
-        <input
-          type="text"
-          class="form-control"
-          id="exampleInputEmail1"
-          v-model="emailRef.val"
-          @blur="validateEmail"
-        />
-        <div class="form-text" v-if="emailRef.error">
-          {{ emailRef.message }}
-        </div>
-      </div>
-      <div class="mb-3">
-        <label class="form-label">邮箱地址</label>
-        <validate-input
-          :rules="emailRules"
-          ref="inputRef"
-          v-model="emailVal"
-          placeholder="请输入邮箱"
-          type="text"
-        ></validate-input>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">密码</label>
-        <validate-input
-          :rules="passwordRules"
-          ref="inputRef"
-          v-model="passwordVal"
-          placeholder="请输入密码"
-          type="password"
-        ></validate-input>
-      </div>
-      <template v-slot:submit>
-        <span class="btn btn-danger">提交2</span>
-      </template>
-    </ValidateForm>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue'
+import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GGGlobalHeader, { UUserProps } from './components/GGGlobalHeader.vue'
-import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
-import ValidateForm from './components/ValidateForm.vue'
 
 const currentUser: UUserProps = {
   isLogin: true,
   name: 'xiaoming'
 }
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是test1的专栏,有一段非常有意思的简介，可以更新一下',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是test2的专栏,有一段非常有意思的简介，可以更新一下',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 3,
-    title: 'test3的专栏',
-    description: '这是test3的专栏,有一段非常有意思的简介，可以更新一下'
-    // avatar:  'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 4,
-    title: 'test4的专栏',
-    description: '这是test4的专栏,有一段非常有意思的简介，可以更新一下',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 5,
-    title: 'test5的专栏',
-    description: '这是test5的专栏,有一段非常有意思的简介，可以更新一下',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
-// 自定义函数了
-const emailRules: RulesProp = [
-  { type: 'required', message: '电子邮箱地址不能为空' },
-  { type: 'email', message: '请输入正确的电子邮箱格式' }
-]
-// 自定义函数了
-const passwordRules: RulesProp = [
-  { type: 'required', message: '密码不能为空' },
-  { type: 'pass', message: '请输入正确的密码(只能包含字母和数字)' }
-]
 
-// 验证邮箱的正则表达式
-const emailReg =
-  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 export default defineComponent({
   name: 'App',
   components: {
-    GGGlobalHeader,
-    ValidateInput,
-    ValidateForm
+    GGGlobalHeader
   },
   setup() {
-    const inputRef = ref<any>()
-    const emailVal = ref('')
-    const emailRef = reactive({
-      val: '',
-      error: false,
-      message: ''
-    })
-    // 下面这个验证是第一个框，可以不要，留着供参考
-    const validateEmail = () => {
-      if (emailRef.val.trim() === '') {
-        emailRef.error = true
-        emailRef.message = '邮箱不能为空'
-      } else if (!emailReg.test(emailRef.val)) {
-        emailRef.error = true
-        emailRef.message = '请输入正确的邮箱'
-      }
-    }
-    // --------------------------------------
-    const passwordVal = ''
-    // 监听子组件的点击结果
-    const onFormSubmit = (result: boolean) => {
-      console.log('669', inputRef.value.validateInput())
-      console.log('1234', result)
-    }
     return {
-      list: testData,
-      currentUser,
-      emailRef,
-      validateEmail,
-      emailRules,
-      emailVal,
-      onFormSubmit,
-      inputRef,
-      passwordRules,
-      passwordVal
+      currentUser
     }
   }
 })
